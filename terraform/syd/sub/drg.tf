5b877088618c111077d38b9d38c77e2d7e137eed
j6js <jackson@j6js.com>
2026-04-28 12:46:03 +1200

    fix: add missing DRG rules to module.syd_sub

--- terraform/syd/sub/drg.tf
+++ terraform/syd/sub/drg.tf
@@ -5,6 +5,18 @@ data "oci_core_drg_attachments" "syd_to_mel_attachments" {
     network_id     = var.rpc_id
 }
 
+resource "oci_core_drg_route_table_route_rule" "syd-local-ipv4" {
+    drg_route_table_id         = var.drg_rt_id
+    destination                = var.syd_ipv4_cidr
+    destination_type           = "CIDR_BLOCK"
+    next_hop_drg_attachment_id = data.oci_core_drg_attachments.syd_to_mel_attachments.drg_attachments[0].id
+}
+resource "oci_core_drg_route_table_route_rule" "syd-local-ipv6" {
+    drg_route_table_id         = var.drg_rt_id
+    destination                = var.syd_ipv6_cidr
+    destination_type           = "CIDR_BLOCK"
+    next_hop_drg_attachment_id = data.oci_core_drg_attachments.syd_to_mel_attachments.drg_attachments[0].id
+}
 resource "oci_core_drg_route_table_route_rule" "syd-remote-ipv4" {
     drg_route_table_id         = var.drg_rt_id
     destination                = var.mel_ipv4_cidr
